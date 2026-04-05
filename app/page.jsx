c98fc2f6c81fe430267939789e8e2cc543a27a7e
beruang <61452341+dimasdew@users.noreply.github.com>
2026-04-05 14:36:38 +0700

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,11 @@
-import Navbar from '@/components/Navbar'
-import Hero from '@/components/Hero'
-import Marquee from '@/components/Marquee'
-import Projects from '@/components/Projects'
-import About from '@/components/About'
-import Contact from '@/components/Contact'
-import Footer from '@/components/Footer'
-import Cursor from '@/components/Cursor'
+import Navbar from '../components/Navbar'
+import Hero from '../components/Hero'
+import Marquee from '../components/Marquee'
+import Projects from '../components/Projects'
+import About from '../components/About'
+import Contact from '../components/Contact'
+import Footer from '../components/Footer'
+import Cursor from '../components/Cursor'
 
 export default function Home() {
   return (
