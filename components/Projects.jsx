6abeff39ccf59d9a6b68cb663f2ca8dfb319e120
beruang <61452341+dimasdew@users.noreply.github.com>
2026-04-06 11:24:40 +0700

    Update Projects.jsx

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -1,8 +1,8 @@
 'use client'
 
-import { useEffect, useRef } from 'react',
-import Link from 'next/link',
-import Image from 'next/image',
+import { useEffect, useRef } from 'react'
+import Link from 'next/link'
+import Image from 'next/image'
 import projects from './projects.data'
 
 function ProjectCard({ project, delay = 0 }) {
